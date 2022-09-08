8f982a0f240aa4ed287b8dc636d93a5756993892
Александр Жилин <modelinterior@yandex.ru>
2022-09-08 17:41:49 +0300

    add Selenium_lesson [2.3.6, 2.4.6, 2.4.8]

---
+++ Selenium_lesson2.3_step6.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+import pyperclip as pc
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+try:
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/redirect_accept.html")
+    browser.find_element(By.TAG_NAME, "button").click()
+    browser.switch_to.window(browser.window_handles[1])
+    x = browser.find_element(By.CSS_SELECTOR, "#input_value")
+    y = calc(x.text)
+    browser.find_element(By.CSS_SELECTOR, '#answer').send_keys(y)
+    browser.find_element(By.TAG_NAME, "button").click()
+    pc.copy(browser.switch_to.alert.text.split(': ')[-1]) #сохранить в буфер
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
---
+++ Selenium_lesson2.4_step8.py
@@ -0,0 +1,30 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import time
+import math
+import pyperclip as pc
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+def open(x):
+    return browser.get(x)
+
+try:
+    browser = webdriver.Chrome()
+    open(" http://suninjuly.github.io/explicit_wait2.html")
+
+    WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), "$100"))
+    browser.find_element(By.ID, "book").click()
+    x = browser.find_element(By.CSS_SELECTOR, "#input_value")
+    y = calc(x.text)
+    browser.find_element(By.CSS_SELECTOR, '#answer').send_keys(y)
+    browser.find_element(By.CSS_SELECTOR, '#solve').click()
+    pc.copy(browser.switch_to.alert.text.split(': ')[-1])  # сохранить в буфер
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
